97d5f2cb30e2e2e642954a314f9d7e140c7763ca
unknown <macjay314@gmail.com>
2020-06-15 10:33:34 -0700

    debug restaurant.cs and restaurantcontroller.cs

---
+++ .history/Controllers/RestaurantController_20200615103125.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using deliver_me_deliveries.Models;
+using Microsoft.AspNetCore.Authorization;
+using System;
+using deliver_me_deliveries.Models;
+
+namespace deliver_me_deliveries.Controller
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+
+
+        // GET api/Restaurant
+        public IActionResult Index()
+        {
+            var allRestaurants = Restaurant.GetAll();
+            return View(allRestaurants);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+          var allRestaurants = Restaurant.GetAll();
+          return View("Index", allRestaurants);
+        }
+    }
+}
